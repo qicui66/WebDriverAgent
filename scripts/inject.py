df9d340293321c19db9b9028301afae03b1ab2ac
qicui66 <910306682@qq.com>
2026-07-07 01:32:48 +0800

    Inject image and video handling methods into commands

--- scripts/inject.py
+++ scripts/inject.py
@@ -3,17 +3,17 @@ import sys
 with open('WebDriverAgentLib/Commands/FBCustomCommands.m', 'r') as f:
     content = f.read()
 
-# 检查是否已经注入过
 if 'handleSaveImageToCameraRoll' in content:
     print('Already injected, skip')
     sys.exit(0)
 
-# 注入路由注册（在 handleSetDeviceAppearance 后面）
-route_line = '    [[FBRoute POST:@"/wda/saveImageToCameraRoll"] respondWithTarget:self action:@selector(handleSaveImageToCameraRoll:)],'
+# === 注入路由（图片 + 视频）===
+route_image = '    [[FBRoute POST:@"/wda/saveImageToCameraRoll"] respondWithTarget:self action:@selector(handleSaveImageToCameraRoll:)],'
+route_video = '    [[FBRoute POST:@"/wda/saveVideoToCameraRoll"] respondWithTarget:self action:@selector(handleSaveVideoToCameraRoll:)],'
 old_route = '    [[FBRoute POST:@"/wda/device/appearance"].withoutSession respondWithTarget:self action:@selector(handleSetDeviceAppearance:)],'
-content = content.replace(old_route, old_route + '\n' + route_line)
+content = content.replace(old_route, old_route + '\n' + route_image + '\n' + route_video)
 
-# 注入方法实现（在 @end 前面）
+# === 注入方法实现 ===
 method_code = """\
 + (id<FBResponsePayload>)handleSaveImageToCameraRoll:(FBRouteRequest *)request
 {
@@ -36,11 +36,31 @@ method_code = """\
   return FBResponseWithOK();
 #endif
 }
+
++ (id<FBResponsePayload>)handleSaveVideoToCameraRoll:(FBRouteRequest *)request
+{
+#if TARGET_OS_TV
+  return FBResponseWithStatus([FBCommandStatus unsupportedOperationErrorWithMessage:@"unsupported" traceback:nil]);
+#else
+  NSString *videoDataBase64 = request.arguments[@"videoData"];
+  if (nil == videoDataBase64) {
+    return FBResponseWithStatus([FBCommandStatus invalidArgumentErrorWithMessage:@"videoData must be provided" traceback:nil]);
+  }
+  NSData *videoData = [[NSData alloc] initWithBase64EncodedString:videoDataBase64 options:NSDataBase64DecodingIgnoreUnknownCharacters];
+  if (nil == videoData) {
+    return FBResponseWithStatus([FBCommandStatus invalidArgumentErrorWithMessage:@"Cannot decode videoData" traceback:nil]);
+  }
+  NSString *fileName = request.arguments[@"fileName"] ?: @"wda_video.mp4";
+  NSString *tempPath = [NSTemporaryDirectory() stringByAppendingPathComponent:fileName];
+  [videoData writeToFile:tempPath atomically:YES];
+  UISaveVideoAtPathToSavedPhotosAlbum(tempPath, nil, nil, nil);
+  return FBResponseWithOK();
+#endif
+}
 """
 content = content.replace('@end', method_code + '\n@end')
 
 with open('WebDriverAgentLib/Commands/FBCustomCommands.m', 'w') as f:
     f.write(content)
 
-count = content.count('handleSaveImageToCameraRoll')
-print('Injection done! Found {} occurrence(s)'.format(count))
+print('Injection done! Image + Video + CameraRoll API')
